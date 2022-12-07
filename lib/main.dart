ca22bd0199c152e147781c89e672a040bd92f589
Ars-eniy-Cheis <mr.4eis@yandex.ru>
2022-12-07 14:39:14 +0300

    Google authorization added

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:routemaster/routemaster.dart';
 import 'package:share_buisness_front_end/pages/debit.dart';
@@ -18,7 +19,19 @@ final routemaster = RoutemasterDelegate(
   routesBuilder: (context) => routes,
 );
 
-void main() {
+void main() async {
+  await Firebase.initializeApp(
+    // Replace with actual values
+    options: FirebaseOptions(
+        apiKey: "AIzaSyBq2HgLyU9y3G68uw_VaTSnxfZpvL8BxIo",
+        authDomain: "openwashing.firebaseapp.com",
+        projectId: "openwashing",
+        storageBucket: "openwashing.appspot.com",
+        messagingSenderId: "931729817638",
+        appId: "1:931729817638:web:0e6a02d606f0389e1c66e7",
+        measurementId: "G-4HD7TZX4BE"
+    ),
+  );
   runApp(MaterialApp.router(
     routerDelegate: routemaster,
     routeInformationParser: RoutemasterParser(),
--- lib/pages/login.dart
+++ lib/pages/login.dart
@@ -1,8 +1,8 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:routemaster/routemaster.dart';
-
 import '../main.dart';
+import '../service/authentication.dart';
 
 class Login extends StatefulWidget {
   const Login({Key? key}) : super(key: key);
@@ -13,32 +13,66 @@ class Login extends StatefulWidget {
 
 class _LoginViewState extends State<Login> {
 
+  bool _isSigningIn = false;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
         backgroundColor: Colors.white70,
-
       body: SafeArea(
         child: Center(
           child: Column(
             children: [
-              Expanded(
-                child: Align(
-                    alignment: FractionalOffset.bottomCenter,
-                    child: SizedBox(
-                        height: 200,
-                        width: 300,
-                        child: Container(
-                          margin: const EdgeInsets.only(bottom: 100.0),
-                          child: ElevatedButton(
-                            style: ButtonStyle(
-                            ),
-                            onPressed: () => {routemaster.push('/profile')},
-                            child: Text("Войти", style: TextStyle(fontSize: 30),),
-                          ),
-                        )
-                    )
-                ),
+              FutureBuilder(
+                future: Authentication.initializeFirebase(context: context),
+                builder: (context, snapshot) {
+                  if (snapshot.hasError) {
+                    return Text(snapshot.error.toString());
+                  } else if (snapshot.connectionState == ConnectionState.done) {
+                    return Expanded(
+                      child: Align(
+                          alignment: FractionalOffset.bottomCenter,
+                          child: _isSigningIn
+                              ? CircularProgressIndicator(
+                            valueColor: AlwaysStoppedAnimation<Color>(Colors.white),
+                          )
+                              :SizedBox(
+                              height: 200,
+                              width: 300,
+                              child: Container(
+                                margin: const EdgeInsets.only(bottom: 100.0),
+                                child: ElevatedButton(
+                                  style: ButtonStyle(
+                                  ),
+                                  onPressed: () async {
+                                    setState(() {
+                                      _isSigningIn = true;
+                                    });
+
+                                    User? user =
+                                    await Authentication.signInWithGoogle(context: context);
+
+                                    setState(() {
+                                      _isSigningIn = false;
+                                    });
+
+                                    if (user != null) {
+                                      routemaster.push('/profile');
+                                    }
+                                  },
+                                  child: Text("Войти", style: TextStyle(fontSize: 30),),
+                                ),
+                              )
+                          )
+                      ),
+                    );
+                  }
+                  return CircularProgressIndicator(
+                    valueColor: AlwaysStoppedAnimation<Color>(
+                      Colors.orange,
+                    ),
+                  );
+                },
               ),
             ],
           ),
