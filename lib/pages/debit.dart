e8449f3680d8808dde33c1e3b51e3fd3b66547f8
Ars-eniy-Cheis <mr.4eis@yandex.ru>
2022-12-11 01:08:45 +0300

    Design integrated

--- lib/pages/debit.dart
+++ lib/pages/debit.dart
@@ -3,6 +3,7 @@ import 'dart:async';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:share_buisness_front_end/pages/side_menu.dart';
 
 import '../main.dart';
@@ -14,7 +15,7 @@ class Debit extends StatefulWidget {
 }
 
 class _DebitState extends State<Debit> {
-
+  var txt = TextEditingController();
   late Timer _everySecond;
   User? user = FirebaseAuth.instance.currentUser;
   late var wash = "1";
@@ -46,88 +47,285 @@ class _DebitState extends State<Debit> {
   @override
   Widget build(BuildContext context) {
     return user != null ? Scaffold(
-        appBar: AppBar(
-          title: Text(''),
-          backgroundColor: Colors.black,),
+        appBar: PreferredSize(
+          preferredSize: Size.fromHeight(50.0),
+          child: AppBar(
+            titleSpacing: -50,
+            //titleSpacing: 220,
+            surfaceTintColor: Colors.white,
+            leadingWidth: 120,
+            leading: Builder(
+              builder: (BuildContext context) {
+                return
+                  Container(
+                    alignment: Alignment.centerLeft,
+                    margin: const EdgeInsets.fromLTRB(0, 18, 60, 0),
+                    padding: const EdgeInsets.fromLTRB(20, 0, 0, 0),
+                    color: Color.fromRGBO(68,68,68, 1),
+
+                    child: IconButton(
+                      icon: const Icon(
+                        Icons.arrow_forward_ios_sharp,
+                      ),
+                      padding: const EdgeInsets.fromLTRB(0, 0, 0, 0),
+                      color: Colors.white,
+                      onPressed: () {
+                        Scaffold.of(context).openDrawer();
+                      },
+                      tooltip: MaterialLocalizations.of(context).openAppDrawerTooltip,
+                    ),
+                  );
+              },
+            ),
+            title: Container(
+                height: 32,
+                alignment: Alignment.centerLeft,
+                margin: const EdgeInsets.fromLTRB(0, 18, 200, 0),
+                padding: const EdgeInsets.fromLTRB(0, 0, 0, 2),
+                color: Color.fromRGBO(227,1,15, 1),
+                child: Center(
+                  child: Text('DIA Electronics',
+                    textAlign: TextAlign.left,
+                    style: TextStyle(
+                        fontFamily: 'Teko',
+                        color: Colors.white,
+                        fontWeight: FontWeight.w300,
+                        fontSize: 30
+                    ),
+                  ),
+                )
+            ),
+            shadowColor: Colors.white,
+            backgroundColor: Colors.white,
+            foregroundColor: Colors.black,
+            iconTheme: IconThemeData(),
+          ),
+        ),
         drawer: SideMenu(),
-        backgroundColor: Colors.white70,
+        backgroundColor: Colors.white,
         body: SafeArea(
-          child: Center(
+          child: Container(
             child: Column(
               children: [
-                Text("Мойка: " + wash, style: TextStyle(fontSize: 30)),
-                SizedBox(height: 30),
-                Text("Пост: " + post, style: TextStyle(fontSize: 30)),
-                SizedBox(height: 30),
-                Text("Баланс: " + balance.toString(), style: TextStyle(fontSize: 30)),
-                SizedBox(height: 30),
-                Text("Будет начислено: " + balance.toString(), style: TextStyle(fontSize: 30)),
-                SizedBox(height: 30),
-                Text("Списать бонусы: " +  _currentSliderValue.toInt().toString(), style: TextStyle(fontSize: 30)),
-                SizedBox(height: 30),
-                SliderTheme(
-                    data: SliderThemeData(
-                      trackHeight: 20,
-                      thumbShape: RoundSliderThumbShape(enabledThumbRadius: 10),
-                      valueIndicatorShape: PaddleSliderValueIndicatorShape(),
-                        valueIndicatorTextStyle: TextStyle(
-                          fontSize: 0,
-                          color: Colors.white,
-                        )
-                    ),
-                    child: Slider(
-                      value: _currentSliderValue.toDouble(),
-                      max: bonus.toDouble(),
-                      divisions: bonus.toInt(),
-                      label: _currentSliderValue.round().toString(),
-                      onChanged: (double value) {
-                        setState(() {
-                          _currentSliderValue = value.toInt();
-                        });
-                      },
+                Center(
+                    child: Column(
+                      children: [
+                        SizedBox(height: 50),
+                        Text("Мойка: " + wash, style: TextStyle(
+                          fontSize: 30,
+                          fontFamily: 'Roboto',
+                          color: Colors.black,
+                        )),
+                        SizedBox(height: 10),
+                        Text("Пост: " + post, style: TextStyle(
+                          fontSize: 24,
+                          fontFamily: 'Roboto',
+                          color: Colors.black,
+                        )),
+
+                      ],
                     )
                 ),
+                Container(
+                  padding: EdgeInsets.fromLTRB(0, 0, 40, 0),
+                  child: Column(
+                    textDirection: TextDirection.ltr,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      SizedBox(height: 30),
+                      Text("Баланс бонусов: " + balance.toString(), style: TextStyle(
+                        fontSize: 18,
+                        fontFamily: 'Roboto',
+                        color: Colors.black,)),
+                      SizedBox(height: 10),
+                      Text("Будет начислено бонусов: " + balance.toString(), style: TextStyle(
+                        fontSize: 18,
+                        fontFamily: 'Roboto',
+                        color: Colors.black,)),
+                    ],
+                  ),
+                ),
                 SizedBox(height: 50),
-                Row(
-                  children: [
-                    Align(
-                      alignment: FractionalOffset.centerLeft,
-                      child: SizedBox(
-                          height: 100,
-                          width: 150,
-                          child: Container(
-                            margin: const EdgeInsets.only(left: 10.0),
-                            child: ElevatedButton(
-                              style: ButtonStyle(
-                              ),
-                              onPressed: () => {},
-                              child: Text("Отмена", style: TextStyle(fontSize: 30),),
-                            ),
-                          )
-                      )
+                Text("Списать бонусы", style: TextStyle(
+                  fontSize: 24,
+                  fontFamily: 'Roboto',
+                  color: Colors.black,
+                )),
+                SizedBox(height: 10),
+                SizedBox(
+                  width: 100,
+                  height: 45,
+                  child: TextField(
+                    style: TextStyle(
+                      fontSize: 18,
+                      fontFamily: 'Roboto',
+                      color: Colors.black,
                     ),
-                    SizedBox(width: 60),
-                    Align(
-                      alignment: FractionalOffset.centerRight,
-                      child: SizedBox(
-                          height: 100,
-                          width: 150,
-                          child: Container(
-                            margin: const EdgeInsets.only(right: 10.0),
-                            child: ElevatedButton(
-                              style: ButtonStyle(
-                              ),
-                              onPressed: () => {},
-                              child: Text("ОК", style: TextStyle(fontSize: 30),),
-                            ),
+                    controller: txt,
+                    keyboardType: TextInputType.number,
+                    onChanged: (text) {
+
+                    },
+                    decoration: InputDecoration(
+                      enabledBorder: OutlineInputBorder(
+                        borderSide: BorderSide(
+                            color: Colors.black, width: 1.0),
+                      ),
+                      focusedBorder: OutlineInputBorder(
+                        borderSide: BorderSide(
+                            color: Colors.black, width: 1.0),
+                      ),
+                    ),
+                    inputFormatters: [
+                      FilteringTextInputFormatter.allow(RegExp(r"[0-9.]")),
+                      TextInputFormatter.withFunction((oldValue, newValue) {
+                        try {
+                          final text = newValue.text;
+                          if (text.isNotEmpty) double.parse(text);
+                          return newValue;
+                        } catch (e) {}
+                        return oldValue;
+                      }),
+                    ],
+                    //
+                  ),
+                ),
+                SizedBox(height: 10),
+                Row(
+                    textDirection: TextDirection.ltr,
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    SizedBox(
+                      height: 30,
+                      width: 30,
+                      child: ElevatedButton(
+                          style: ButtonStyle(
+                              backgroundColor: MaterialStatePropertyAll<Color>(Color.fromRGBO(227,1,15, 1)),
+                              shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+                                  RoundedRectangleBorder(
+                                    borderRadius: BorderRadius.circular(1),
+                                  )
+                              )
+                          ),
+                          onPressed: () {},
+                          child: Text('-',
+                          style: TextStyle(
+                            fontSize: 25,
+                            fontFamily: 'Roboto',
+                            color: Colors.white,),
+                          textAlign: TextAlign.center,
                           )
+                      ),
+                    ),
+                    SliderTheme(
+                        data: SliderThemeData(
+                            activeTrackColor: Colors.black,
+                            inactiveTrackColor: Colors.black,
+                            thumbColor: Color.fromRGBO(227,1,15, 1),
+                            trackHeight: 3,
+                            trackShape: RectangularSliderTrackShape(),
+                            thumbShape: RoundSliderThumbShape(enabledThumbRadius: 10),
+                            valueIndicatorShape: RectangularSliderValueIndicatorShape(),
+                            valueIndicatorColor: Colors.black,
+
+                        ),
+                        child: Slider(
+                          value: _currentSliderValue.toDouble(),
+                          max: bonus.toDouble(),
+                          divisions: bonus.toInt(),
+                          label: _currentSliderValue.round().toString(),
+                          onChanged: (double value) {
+                            setState(() {
+                              _currentSliderValue = value.toInt();
+                            });
+                          },
+                        )
+                    ),
+                    SizedBox(
+                      height: 30,
+                      width: 30,
+                      child: ElevatedButton(
+                        style: ButtonStyle(
+                            backgroundColor: MaterialStatePropertyAll<Color>(Color.fromRGBO(227,1,15, 1)),
+                            shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+                                RoundedRectangleBorder(
+                                  borderRadius: BorderRadius.circular(1),
+                                )
+                            )
+                        ),
+                          onPressed: ()  {},
+                          child: Text('+',
+                            textAlign: TextAlign.center,
+                              style: TextStyle(
+                                fontSize: 25,
+                                fontFamily: 'Roboto',
+                                color: Colors.white,),
+                          ),
+
                       ),
                     )
                   ],
+                ),
+                SizedBox(height: 70),
+                Row(
+                  textDirection: TextDirection.ltr,
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    SizedBox(
+                        height: 40,
+                        width: 150,
+                        child: Container(
+                          margin: const EdgeInsets.only(left: 10.0),
+                          child: ElevatedButton(
+                            style: ButtonStyle(
+                                side: MaterialStateBorderSide.resolveWith((states) => BorderSide(width: 3, color: Color.fromRGBO(227,1,15, 1))),
+                                backgroundColor: MaterialStatePropertyAll<Color>(Colors.white),
+                                shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+                                    RoundedRectangleBorder(
+
+                                      borderRadius: BorderRadius.circular(1),
+                                    )
+                                )
+                            ),
+                            onPressed: () => {},
+                            child: Text("Отмена", style: TextStyle(
+                              fontSize: 18,
+                              fontFamily: 'RobotoCondensed',
+                              color: Colors.black,
+                              fontWeight: FontWeight.w700,
+                            ),),
+                          ),
+                        )
+                    ),
+                    SizedBox(
+                        height: 40,
+                        width: 150,
+                        child: Container(
+                          margin: const EdgeInsets.only(right: 10.0),
+                          child: ElevatedButton(
+                            style: ButtonStyle(
+                                backgroundColor: MaterialStatePropertyAll<Color>(Color.fromRGBO(227,1,15, 1)),
+                                shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+                                    RoundedRectangleBorder(
+                                      borderRadius: BorderRadius.circular(1),
+                                    )
+                                )
+                            ),
+                            onPressed: () => {},
+                            child: Text("Подтвердить", style: TextStyle(
+                              fontSize: 18,
+                              fontFamily: 'RobotoCondensed',
+                              color: Colors.white,
+                              fontWeight: FontWeight.w700,
+                            ),),
+                          ),
+                        )
+                    ),
+                  ],
                 )
               ],
-            )
-    )
+            ),
+          )
         )
     ) :
         Container();
--- lib/pages/login.dart
+++ lib/pages/login.dart
@@ -1,6 +1,8 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:float_column/float_column.dart';
+
 import '../main.dart';
 import '../service/authentication.dart';
 
@@ -22,7 +24,63 @@ class _LoginViewState extends State<Login> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        backgroundColor: Colors.white70,
+
+        appBar: PreferredSize(
+          preferredSize: Size.fromHeight(50.0),
+          child: AppBar(
+            automaticallyImplyLeading: false,
+            titleSpacing: 0,
+            surfaceTintColor: Colors.white,
+            leadingWidth: 30,
+            leading: Builder(
+              builder: (BuildContext context) {
+                return
+                  Container(
+                    alignment: Alignment.centerLeft,
+                    margin: const EdgeInsets.fromLTRB(0, 18, 60, 0),
+                    padding: const EdgeInsets.fromLTRB(20, 0, 0, 0),
+                    color: Color.fromRGBO(68,68,68, 1),
+
+                    child: IconButton(
+                      icon: const Icon(
+                        Icons.arrow_forward_ios_sharp,
+                      ),
+                      padding: const EdgeInsets.fromLTRB(0, 0, 0, 0),
+                      color: Colors.white,
+                      onPressed: () {
+                        Scaffold.of(context).openDrawer();
+                      },
+                      tooltip: MaterialLocalizations.of(context).openAppDrawerTooltip,
+                    ),
+                  );
+              },
+            ),
+            title: Container(
+                height: 32,
+                alignment: Alignment.centerLeft,
+                margin: const EdgeInsets.fromLTRB(0, 18, 190, 0),
+                padding: const EdgeInsets.fromLTRB(0, 0, 0, 2),
+                color: Color.fromRGBO(227,1,15, 1),
+                child: Center(
+                  child: Text('DIA Electronics',
+                    textAlign: TextAlign.left,
+                    style: TextStyle(
+                      fontFamily: 'Teko',
+                        color: Colors.white,
+                        fontWeight: FontWeight.w300,
+                        fontSize: 30
+                    ),
+                  ),
+                )
+            ),
+            shadowColor: Colors.white,
+            backgroundColor: Colors.white,
+            foregroundColor: Colors.black,
+            iconTheme: IconThemeData(),
+          ),
+        ),
+
+        backgroundColor: Colors.white,
       body: SafeArea(
         child: Center(
           child: Column(
@@ -37,35 +95,80 @@ class _LoginViewState extends State<Login> {
                       child: Align(
                           alignment: FractionalOffset.bottomCenter,
                           child: _isSigningIn
-                              ? CircularProgressIndicator(
-                            valueColor: AlwaysStoppedAnimation<Color>(Colors.black),
+                              ? Column(
+                            children: [
+                              SizedBox(height: 300,),
+                              CircularProgressIndicator(
+                                valueColor: AlwaysStoppedAnimation<Color>(Colors.black),
+                              ),
+                            ],
                           )
-                              :SizedBox(
+                              : SizedBox(
                               height: 150,
                               width: 300,
                               child: Container(
-                                margin: const EdgeInsets.only(bottom: 100.0),
-                                child: ElevatedButton(
-                                  style: ButtonStyle(
-                                  ),
-                                  onPressed: () async {
-                                    setState(() {
-                                      _isSigningIn = true;
-                                    });
+                                //margin: const EdgeInsets.only(bottom: 100.0),
+                                child: Column(
+                                  children: [
+                                    SizedBox(
+                                      height: 50,
+                                      width: 300,
+                                      child: ElevatedButton(
+                                        style: ButtonStyle(
+                                            backgroundColor: MaterialStatePropertyAll<Color>(Color.fromRGBO(227,1,15, 1)),
+                                            shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+                                                RoundedRectangleBorder(
+                                                  borderRadius: BorderRadius.circular(1),
+                                                )
+                                            )
+                                        ),
+                                        onPressed: () async {
+                                          setState(() {
+                                            _isSigningIn = true;
+                                          });
 
-                                    User? user =
-                                    await Authentication.signInWithGoogle(context: context);
+                                          User? user =
+                                          await Authentication.signInWithGoogle(context: context);
 
-                                    setState(() {
-                                      _isSigningIn = false;
-                                    });
+                                          setState(() {
+                                            _isSigningIn = false;
+                                          });
 
-                                    if (user != null) {
-                                      routemaster.push('/profile');
-                                    }
-                                  },
-                                  child: Text("Войти", style: TextStyle(fontSize: 25),),
-                                ),
+                                          if (user != null) {
+                                            routemaster.push('/debit');
+                                          }
+                                        },
+                                        child: Text("Войти", style:
+                                        TextStyle(
+                                            fontSize: 22,
+                                            fontFamily: 'RobotoCondensed',
+                                            color: Colors.white,
+                                            fontWeight: FontWeight.w700,
+                                        ),
+                                        ),
+                                      ),
+                                    ),
+                                    SizedBox(height: 20,),
+                                    Row(
+                                      mainAxisAlignment: MainAxisAlignment.center,
+                                      children: [
+                                        Text(
+                                            'Вход с  ',
+                                            style: TextStyle(
+                                              fontSize: 22,
+                                              fontFamily: 'RobotoCondensed',
+                                              fontWeight: FontWeight.w400,
+                                            )
+                                        ),
+                                        Image.asset(
+                                          'google_logo.png',
+                                          height: 50,
+                                          width: 50,
+                                        )
+                                      ],
+                                    )
+                                  ],
+                                )
                               )
                           )
                       ),
